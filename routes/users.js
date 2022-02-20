f6f8b5d8daaabe5a4bb4e9589737a35f27bb6558
Ian Chiu <nk9710632@gmail.com>
2022-02-20 17:13:39 +0800

    feat: add API to query user data from database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const { Client } = require('pg');
+const client = new Client({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  database: 'postgres'
+});
+client.connect();
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userId', function(req, res, next) {
+  const id = req.params.userId;
+  client.query(`SELECT * FROM users WHERE id = ${id}`, (err, data) => {
+    const user = data.rows[0];
+    res.json({
+      firstName: user.first_name,
+      lastName: user.last_name,
+    });
+  });
 });
 
 module.exports = router;
